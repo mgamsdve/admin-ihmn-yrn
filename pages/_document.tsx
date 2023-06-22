d99a601b7756357cb65c118f85c975f2a0f9e234
mgamsdve <mael.dayani.poty@gmail.com>
2023-06-22 20:43:31 +0200

    Image optiomisation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
+            <meta name="google-site-verification" content="mg-duFqkJC9FdYQIJ7vQTpP4ASnGtDrlgYe-Ix4yCwM" />
             <title>Ihmn Admin</title>
             <link rel="icon" href="ihmnIcon.ico" />
 
